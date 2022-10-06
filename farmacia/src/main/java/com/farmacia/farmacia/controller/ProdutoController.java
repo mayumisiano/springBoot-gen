a2cdf0840a2dcd0e7eb74f990dbffe85b55c6e50
Paula Mayumi <mayumi_siano10@hotmail.com>
2022-10-06 13:40:25 -0300

    Atualizando os métodos

--- farmacia/src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
+++ farmacia/src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.farmacia.farmacia.model.Produto;
+import com.farmacia.farmacia.repository.CategoriaRepository;
 import com.farmacia.farmacia.repository.ProdutoRepository;
 
 @RestController
@@ -29,6 +31,9 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll() {
@@ -47,13 +52,25 @@ public class ProdutoController {
 		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		if(categoriaRepository.existsById(produto.getCategoria().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(produtoRepository.save(produto));
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+		
 	@PutMapping
 	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
-		return produtoRepository.findById(produto.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-						.body(produtoRepository.save(produto)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());	
-				
+		if(produtoRepository.existsById(produto.getId())) {
+			if(categoriaRepository.existsById(produto.getCategoria().getId()))
+				return ResponseEntity.status(HttpStatus.OK) 
+						.body(produtoRepository.save(produto));			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();			
+		}
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();				
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
